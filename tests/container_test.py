4170c9171965ccc2c89de01f55604b2985648180
Esben Haabendal <esben@haabendal.dk>
2015-10-19 20:29:20 +0200

    container: Allow constructor to take inspect response
    Signed-off-by: Esben Haabendal <esben@haabendal.dk>

--- tests/container_test.py
+++ tests/container_test.py
@@ -51,3 +51,192 @@ class tests(unittest.case.TestCase):
         self.assertEqual(container.sizerw, 12288)
         self.assertEqual(container.sizerootfs, 0)
 
+
+    def test_init_inspect_response(self):
+        container = DockerContainer(
+            self.client, id='123456789abcdef'*4, inspect_response={
+                "AppArmorProfile": "",
+                "Args": [
+                    "-c",
+                    "exit 9"
+                ],
+                "Config": {
+                    "AttachStderr": True,
+                    "AttachStdin": False,
+                    "AttachStdout": True,
+                    "Cmd": [
+                        "/bin/sh",
+                        "-c",
+                        "exit 9"
+                    ],
+                    "Domainname": "",
+                    "Entrypoint": None,
+                    "Env": [
+                        "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+                    ],
+                    "ExposedPorts": None,
+                    "Hostname": "ba033ac44011",
+                    "Image": "ubuntu",
+                    "Labels": {
+                        "com.example.vendor": "Acme",
+                        "com.example.license": "GPL",
+                        "com.example.version": "1.0"
+                    },
+                    "MacAddress": "",
+                    "NetworkDisabled": False,
+                    "OnBuild": None,
+                    "OpenStdin": False,
+                    "StdinOnce": False,
+                    "Tty": False,
+                    "User": "",
+                    "Volumes": None,
+                    "WorkingDir": ""
+                },
+                "Created": "2015-01-06T15:47:31.485331387Z",
+                "Driver": "devicemapper",
+                "ExecDriver": "native-0.2",
+                "ExecIDs": None,
+                "HostConfig": {
+                    "Binds": None,
+                    "BlkioWeight": 0,
+                    "CapAdd": None,
+                    "CapDrop": None,
+                    "ContainerIDFile": "",
+                    "CpusetCpus": "",
+                    "CpusetMems": "",
+                    "CpuShares": 0,
+                    "CpuPeriod": 100000,
+                    "Devices": [],
+                    "Dns": None,
+                    "DnsSearch": None,
+                    "ExtraHosts": None,
+                    "IpcMode": "",
+                    "Links": None,
+                    "LxcConf": [],
+                    "Memory": 0,
+                    "MemorySwap": 0,
+                    "OomKillDisable": False,
+                    "NetworkMode": "bridge",
+                    "PortBindings": {},
+                    "Privileged": False,
+                    "ReadonlyRootfs": False,
+                    "PublishAllPorts": False,
+                    "RestartPolicy": {
+                        "MaximumRetryCount": 2,
+                        "Name": "on-failure"
+                    },
+                    "LogConfig": {
+                        "Config": None,
+                        "Type": "json-file"
+                    },
+                    "SecurityOpt": None,
+                    "VolumesFrom": None,
+                    "Ulimits": [{}]
+                },
+                "HostnamePath": "/var/lib/docker/containers/ba033ac4401106a3b513bc9d639eee123ad78ca3616b921167cd74b20e25ed39/hostname",
+                "HostsPath": "/var/lib/docker/containers/ba033ac4401106a3b513bc9d639eee123ad78ca3616b921167cd74b20e25ed39/hosts",
+                "LogPath": "/var/lib/docker/containers/1eb5fabf5a03807136561b3c00adcd2992b535d624d5e18b6cdc6a6844d9767b/1eb5fabf5a03807136561b3c00adcd2992b535d624d5e18b6cdc6a6844d9767b-json.log",
+                "Id": "ba033ac4401106a3b513bc9d639eee123ad78ca3616b921167cd74b20e25ed39",
+                "Image": "04c5d3b7b0656168630d3ba35d8889bd0e9caafcaeb3004d2bfbc47e7c5d35d2",
+                "MountLabel": "",
+                "Name": "/boring_euclid",
+                "NetworkSettings": {
+                    "Bridge": "",
+                    "Gateway": "",
+                    "IPAddress": "",
+                    "IPPrefixLen": 0,
+                    "MacAddress": "",
+                    "PortMapping": None,
+                    "Ports": None
+                },
+                "Path": "/bin/sh",
+                "ProcessLabel": "",
+                "ResolvConfPath": "/var/lib/docker/containers/ba033ac4401106a3b513bc9d639eee123ad78ca3616b921167cd74b20e25ed39/resolv.conf",
+                "RestartCount": 1,
+                "State": {
+                    "Error": "",
+                    "ExitCode": 9,
+                    "FinishedAt": "2015-01-06T15:46:36.080254511Z",
+                    "OOMKilled": False,
+                    "Paused": False,
+                    "Pid": 0,
+                    "Restarting": False,
+                    "Running": False,
+                    "StartedAt": "2015-01-06T15:43:32.072697474Z"
+                },
+                "Mounts": [
+                    {
+                        "Source": "/data",
+                        "Destination": "/data",
+                        "Mode": "ro,Z",
+                        "RW": False
+                    }
+                ]
+            })
+        self.assertIsNotNone(container, DockerContainer)
+        et = container.state.execution_time
+        self.assertEqual(et.days, 0)
+        self.assertEqual(et.seconds, 184)
+        self.assertEqual(et.microseconds, 7557)
+
+    def test_init_inspect_response_partial(self):
+        container = DockerContainer(
+            self.client, id='123456789abcdef'*4, inspect_response={
+                "AppArmorProfile": "",
+                "Args": [
+                    "-c",
+                    "exit 9"
+                ],
+                "Config": {
+                    "Cmd": [
+                        "/bin/sh",
+                        "-c",
+                        "exit 9"
+                    ],
+                    "Entrypoint": None,
+                    "Env": [
+                        "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+                    ],
+                    "Hostname": "ba033ac44011",
+                    "Image": "ubuntu",
+                    "Labels": {},
+                    "Tty": False,
+                    "User": "",
+                    "Volumes": None,
+                    "WorkingDir": ""
+                },
+                "Created": "2015-01-06T15:47:31.485331387Z",
+                "HostConfig": {
+                    "Binds": None,
+                    "Ulimits": [{}]
+                },
+                "HostnamePath": "/var/lib/docker/containers/ba033ac4401106a3b513bc9d639eee123ad78ca3616b921167cd74b20e25ed39/hostname",
+                "HostsPath": "/var/lib/docker/containers/ba033ac4401106a3b513bc9d639eee123ad78ca3616b921167cd74b20e25ed39/hosts",
+                "LogPath": "/var/lib/docker/containers/1eb5fabf5a03807136561b3c00adcd2992b535d624d5e18b6cdc6a6844d9767b/1eb5fabf5a03807136561b3c00adcd2992b535d624d5e18b6cdc6a6844d9767b-json.log",
+                "Id": "ba033ac4401106a3b513bc9d639eee123ad78ca3616b921167cd74b20e25ed39",
+                "Image": "04c5d3b7b0656168630d3ba35d8889bd0e9caafcaeb3004d2bfbc47e7c5d35d2",
+                "MountLabel": "",
+                "Name": "/boring_euclid",
+                "NetworkSettings": {
+                    "Bridge": "",
+                    "Gateway": "",
+                    "IPAddress": "",
+                    "IPPrefixLen": 0,
+                    "MacAddress": "",
+                    "PortMapping": None,
+                    "Ports": None
+                },
+                "Path": "/bin/sh",
+                "ProcessLabel": "",
+                "ResolvConfPath": "/var/lib/docker/containers/ba033ac4401106a3b513bc9d639eee123ad78ca3616b921167cd74b20e25ed39/resolv.conf",
+                "RestartCount": 1,
+                "Mounts": [
+                    {
+                        "Source": "/data",
+                        "Destination": "/data",
+                        "Mode": "ro,Z",
+                        "RW": False
+                    }
+                ]
+            })
+        self.assertIsNotNone(container, DockerContainer)
--- xd/docker/container.py
+++ xd/docker/container.py
@@ -3,22 +3,43 @@ log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
 from xd.docker.image import *
+from xd.docker.datetime import *
 
 
 __all__ = ['DockerContainer']
+
+
+class DockerContainerState(object):
+    def __init__(self, state):
+        assert state is not None
+        self.error = state.get('Error', None)
+        self.exit_code = state.get('ExitCode', None)
+        self.finished_at = strptime(state.get('FinishedAt', None))
+        self.oom_killed = state.get('OOMKilled', None)
+        self.paused = state.get('Paused', None)
+        self.pid = state.get('Pid', None)
+        self.restarting = state.get('Restarting', None)
+        self.running = state.get('Running', None)
+        self.started_at = strptime(state.get('StartedAt', None))
+
+    @property
+    def execution_time(self):
+        return (self.finished_at - self.started_at)
 
 
 class DockerContainer(object):
     """Docker container."""
 
     def __init__(self, client, id=None, name=None,
-                 list_response=None):
+                 list_response=None, inspect_response=None):
         """Docker container concstructor."""
         self.client = client
         self.id = id
         self.name = name
         if list_response:
             self._parse_list_response(list_response)
+        if inspect_response:
+            self._parse_inspect_response(inspect_response)
 
     LIST_RESPONSE_ATTRS = (
         'Id', 'Names', 'Image', 'Command', 'Created', 'Status', 'Ports',
@@ -38,6 +59,41 @@ class DockerContainer(object):
                 (value,) = value
             setattr(self, name.lower(), value)
 
+    INSPECT_RESPONSE_ATTRS = (
+        'AppArmorProfile', 'Args', 'Created', 'Driver', 'ExecDriver',
+        'ExecIDs', 'HostnamePath', 'LogPath', 'Id', 'Image', 'MountLabel',
+        'Name', 'Path', 'ProcessLabel', 'ResolveConfPath', 'RestartCount',
+        'Mounts')
+
+    def _parse_inspect_response(self, response):
+        response = response.copy()
+        for name in self.INSPECT_RESPONSE_ATTRS:
+            try:
+                value = response.pop('Id')
+            except KeyError:
+                continue
+            setattr(self, name.lower(), value)
+        self._parse_config(response.get('Config'))
+        self._parse_host_config(response.get('HostConfig'))
+        self._parse_network_settings(response.get('NetworkSettings'))
+        if 'State' in response:
+            self.state = DockerContainerState(response.get('State'))
+
+
+    CONFIG_ATTRS = (
+        'AttachStderr', 'AttachStdin', 'AttachStdout', 'Cmd', 'Domainname',
+        'Entrypoint', 'Env', 'ExposedPorts', 'Hostname', 'Image', 'Labels',
+        'MacAddress', 'NetworkDisabled', 'OnBuild', 'OpenStdin', 'StdinOnce',
+        'Tty', 'User', 'Volumes', 'WorkingDir')
+
+    def _parse_config(self, response):
+        for name in self.CONFIG_ATTRS:
+            try:
+                value = response[name]
+            except KeyError:
+                continue
+            setattr(self, name.lower(), value)
+
     HOST_CONFIG_ATTRS = (
         'Binds', 'BlkioWeight', 'CapAdd', 'CapDrop', 'ContainerIDFile',
         'CpusetCpus', 'CpusetMems', 'CpuShares', 'CpuPeriod', 'Devices', 'Dns',
@@ -48,6 +104,18 @@ class DockerContainer(object):
 
     def _parse_host_config(self, response):
         for name in self.HOST_CONFIG_ATTRS:
+            try:
+                value = response[name]
+            except KeyError:
+                continue
+            setattr(self, name.lower(), value)
+
+    NETWORK_SETTINGS_ATTRS = (
+        "Bridge", "Gateway", "IPAddress", "IPPrefixLen", "MacAddress",
+        "PortMapping", "Ports")
+
+    def _parse_network_settings(self, response):
+        for name in self.NETWORK_SETTINGS_ATTRS:
             try:
                 value = response[name]
             except KeyError:
