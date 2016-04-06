d8eda0d538471dfee26bb7c7d84e1e622ceb940f
Esben Haabendal <esben@haabendal.dk>
2016-04-06 12:12:27 +0200

    CI: Reduce time spent on pip install
    Only install additional requirements in steps where they are actually
    needed.
    
    Signed-off-by: Esben Haabendal <esben@haabendal.dk>

--- ci-step.sh
+++ ci-step.sh
@@ -5,14 +5,17 @@ shift
 case "$suite" in
 
     unit)
+	pip install -r tests/requirements.txt
 	`dirname $0`/unit-test.sh --cov-report=xml
 	;;
 
     integration)
+	pip install -r tests/requirements.txt
 	`dirname $0`/integration-test.sh --cov-report=xml
 	;;
 
     style)
+	pip install flake8
 	flake8 --show-source --statistics --count xd/docker
 	;;
 
